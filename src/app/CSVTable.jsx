a7e04930de9c3be9425d9d4ae6bcdd208fa89d1f
Aditya Kulshrestha <adi.kul358@gmail.com>
2025-04-13 17:29:44 +0530

    Query pipeline v2 completed w/ samples

--- src/app/CSVTable.jsx
+++ src/app/CSVTable.jsx
@@ -1,9 +1,21 @@
 "use client"
-import CsvToHtmlTable from "react-csv-to-table"
+import Papa from "papaparse";
 
-export default function CSVTable({ csvRes }) {
-    const rows = csvRes.trim().split("\n");
-    const tableData = rows.map((row) => row.split(","));
+export default function CSVTable({ csvData }) {
+    let csvRes = "Test case id,Title,Description,Pre-conditions,Test steps,Expected results,ETA,Priority,Test type,Env,Screen,Verified,Category\n"
+
+    for (const c of csvData) {
+        csvRes += c.match(/^```csv\nTest case id,Title,Description,Pre-conditions,Test steps,Expected results,ETA,Priority,Test type,Env,Screen,Verified,Category\n([\S\s]+)```/)[1]
+    }
+
+    const parsed = Papa.parse(csvRes.trim(), {
+        header: false,
+        skipEmptyLines: true,
+      });
+    
+    const tableData = parsed.data;
+    
+    console.log(csvRes)
 
     return (
         <div className="overflow-autow">
@@ -22,7 +34,7 @@ export default function CSVTable({ csvRes }) {
                         <tr key={rowIndex} className="hover:bg-blue-300/30 transition-colors ease-in">
                             {row.map((cell, cellIndex) => (
                                 <td key={cellIndex} className="border px-4 py-2">
-                                    {cell}
+                                    {cellIndex == 0 ? `TD_${String(rowIndex + 1).padStart(3, '0')}` : cell}
                                 </td>
                             ))}
                         </tr>
